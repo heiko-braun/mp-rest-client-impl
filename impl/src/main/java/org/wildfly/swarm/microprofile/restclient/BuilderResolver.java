64d4e15bb6fa379c51bdae670586c4c8b8aed9a2
Heiko Braun <ike.braun@googlemail.com>
2018-01-22 19:30:47 +0100

    Standalone client that wraps RESTeasy

---
+++ impl/src/main/java/org/wildfly/swarm/microprofile/restclient/BuilderResolver.java
@@ -0,0 +1,14 @@
+package org.wildfly.swarm.microprofile.restclient;
+
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.eclipse.microprofile.rest.client.spi.RestClientBuilderResolver;
+
+/**
+ * Created by hbraun on 22.01.18.
+ */
+public class BuilderResolver extends RestClientBuilderResolver {
+    @Override
+    public RestClientBuilder newBuilder() {
+        return new BuilderImpl();
+    }
+}
